c61a69c65d85b5bc468ab5a2c9ed506fc9734f75
Hady Eslam <abdoaslam000@gmail.com>
2026-09-04 00:44:58 +0300

    Fixes

--- deployment/prod/setup_vps/setup_repo.sh
+++ deployment/prod/setup_vps/setup_repo.sh
@@ -57,7 +57,7 @@ sudo -u "$TARGET_USER" bash << EOF
 
 
     # STEP -> Install FE dependencies
-    export NVM_DIR="/root/.nvm"
+    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
     nvm install v20
